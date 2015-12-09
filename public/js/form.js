5f7b1b8442d4fe59e9d94f91369cc2621fef4f4b
Andrew Dushane <andrewdushane@gmail.com>
2015-12-09 08:56:32 -0600

    form style updates

--- public/js/form.js
+++ public/js/form.js
@@ -1,7 +1,17 @@
 $('document').ready( function() {
+  // highlight checked sources
+  $('input[type="checkbox"]:checked').prev().addClass('checked-image');
+  // check checkbox when image is clicked
   $(".form-image").click( function() {
-    $(this).next().trigger('click');
+    var checkbox = $(this).next();
+    checkbox.trigger('click');
+    if( checkbox.is(':checked') ) {
+      $(this).addClass('checked-image');
+    } else {
+      $(this).removeClass('checked-image');
+    }
   });
+  // check checkbox when label is clicked
   $('input[type="checkbox"] + label').click( function() {
     $(this).prev().trigger('click');
   });
